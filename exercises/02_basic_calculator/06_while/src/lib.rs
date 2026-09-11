94c1eb6e206d2109551808a49bba5b63d40e5129
Allen <allenchen@iscoollab.com>
2026-09-11 16:50:24 +0800

    Exercise 02 finished

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,17 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n == 0{
+        1
+    } else {
+        let mut ans = 1;
+        let mut n = n;
+        while n > 0{
+            ans = ans * n;
+            n = n - 1;
+        }
+        ans
+    }
 }
 
 #[cfg(test)]
